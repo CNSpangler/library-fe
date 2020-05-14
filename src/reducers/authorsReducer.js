fbd7d9726e6c4e3b95bc4c8a6b863646e406f5a5
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-13 18:26:06 -0700

    debugged so AuthorForm renders

--- src/reducers/authorsReducer.js
+++ src/reducers/authorsReducer.js
@@ -1,6 +1,6 @@
 import { ADD_AUTHOR } from "../actions/authorActions";
 
-export default function reducer(state, action) {
+export default function reducer(state = [], action) {
   switch(action.type) {
     case ADD_AUTHOR:
       return [...state, action.payload];
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import authors from './authorsReducer';
+
+
 
 export default combineReducers({
-  
+  authors
 });
